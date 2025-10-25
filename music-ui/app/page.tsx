5e6b1cca854eaa63d8c0e1028700f0f0778cb382
tony <tonykcho3527657@gmail.com>
2025-10-25 23:44:50 +0800

    add watchtower

--- music-ui/app/page.tsx
+++ music-ui/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <div className='flex flex-row items-center justify-center flex-1'>
-      <p className='text-xl'>Welcome to Music Collection</p>
+      <p className='text-xl'>Welcome to Music Collection (Testing Watch Tower)</p>
     </div>
   );
 }
